486926b3d042f759beea52f5a8fdfc9e35ffdeb8
radhikasoni <sradhika1998@gmail.com>
2025-01-07 00:10:58 +0530

    update

--- stock_prediction_numpy.py
+++ stock_prediction_numpy.py
@@ -1,17 +1,3 @@
-# Copyright 2020-2024 Jordi Corbilla. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
 import os
 
 import numpy as np
@@ -19,7 +5,7 @@ from datetime import timedelta
 import random
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
-from datetime import datetime
+from datetime import timedelta, time
 import yfinance as yf
 
 
